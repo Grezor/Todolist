92773082b3858e8af8de2119e2428fed14123515
Grezor <grezor.gamer@gmail.com>
2020-01-02 16:57:39 +0100

    new repo

---
+++ public/callApi.js
@@ -0,0 +1,133 @@
+// import { func } from "joi";
+
+const app = document.getElementById('root')
+
+const container = document.createElement('ul')
+container.setAttribute('id', 'incomplete-tasks')
+
+app.appendChild(container);
+
+var request = new XMLHttpRequest();
+afficherGlobal();
+
+
+/**
+ * Affiche le tableau de la todolist au chargement
+ */
+function afficherGlobal() {
+
+    var request = new XMLHttpRequest();
+    request.open('GET', 'http://localhost:7778/api/todolists', true);
+
+    request.onload = function () {
+        var data = JSON.parse(this.response);
+
+        if (request.status >= 200 && request.status < 400) {
+            statusOk(data)
+        } else {
+            statusError()
+        }
+    }
+    request.send();
+
+}
+
+/**
+ * 
+ * @param {*} data est le nom du tableau
+ */
+function statusOk(data) {
+    data.forEach(afaire => {
+        const li = document.createElement('li');
+        li.setAttribute('class', 'afaire')
+
+        const input = document.createElement('INPUT')
+        input.setAttribute("type", "checkbox");
+
+        const label = document.createElement("LABEL");
+        label.textContent = afaire.name
+        label.setAttribute('class', 'label');
+
+        const inputText = document.createElement('INPUT')
+        inputText.setAttribute("type", "text");
+        label.setAttribute('class', 'label');
+
+        const BtnEdit = document.createElement('INPUT')
+        BtnEdit.setAttribute("type", "button");
+        BtnEdit.setAttribute("class", "edit");
+        BtnEdit.setAttribute("value", "EDIT");
+        BtnEdit.setAttribute("id", "edit");
+        el = BtnEdit.dataset.name
+        el.dataset.name = "bla";
+
+        const BtnDelete = document.createElement('INPUT')
+        BtnDelete.setAttribute("type", "button");
+        BtnEdit.setAttribute("class", "delete");
+        BtnDelete.setAttribute("value", "DELETE");
+
+
+        container.appendChild(li);
+        li.appendChild(input);
+        li.appendChild(label);
+        li.appendChild(inputText);
+        li.appendChild(BtnEdit);
+        li.appendChild(BtnDelete);
+    });
+}
+
+function statusError() {
+    const errorMessage = document.createElement('marquee');
+    errorMessage.textContent = `ERREUR !`;
+    app.appendChild(errorMessage);
+}
+
+function ajout(taskValue) {
+    var request = new XMLHttpRequest();
+    // requete serveur methode post
+    request.open('POST', 'http://localhost:7778/api/todolists/', true);
+    request.setRequestHeader("Content-Type", "application/json");
+    // l'objet est modifier
+    var obj = { name: taskValue }
+    var jsonObjet = JSON.stringify(obj);
+    // si la requete a le status 200 tu affiche
+    request.send(jsonObjet);
+
+    // afficherGlobal 
+
+}
+
+function SupprimerElement() {
+    var request = new XMLHttpRequest();
+    // requete serveur methode post
+    request.open('DELETE', 'http://localhost:7778/api/todolists/', true);
+    request.setRequestHeader("Content-Type", "application/json");
+
+
+    const todolist = todolists.find(c => c.id === parseInt(request.params.id));
+
+
+    //delete
+
+    // La méthode indexOf() renvoie le premier indice pour lequel on trouve un élément donné dans un tableau
+    const index = todolists.indexOf(todolist);
+    // splite permet de diviser une chaine à partir d'un séparateurs
+    todolists.splice(index, 1);
+    // return la list du tableau
+    console.log("id :  " + request.params.id + " ========== element supprimer ");
+    request.send(todolist)
+
+
+}
+
+function Edit() {
+
+}
+
+function Update() {
+
+}
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,146 @@
+const Joi = require('joi')
+const express = require('express')
+const app = express()
+// tableau de ma todo (sans bdd)
+const todolists = [
+  {id: 1, name: 'test1'},
+  {id: 2, name: 'test2'},
+  {id: 3, name: 'test3'}
+];
+
+app.use(express.static('TodoList_Server/public'))
+app.use(express.json());
+
+/**====================================================================================================
+ * GET 
+====================================================================================================*/
+/**
+ * 
+ */
+app.get('/', (req, res) => {
+  res.sendfile('TodoList_Server/public/index.html');
+});
+
+/* ------------------------- affiche toute les todolists ------------------------ */
+app.get('/api/todolists', (req, res) => {
+  res.send(todolists);
+});
+
+/* ------------------- affiche la todo avec l'id en paramètre ------------------ */
+
+app.get('/api/todolists/:id', (req, res) => {
+  const todolist = todolists.find( c => c.id === parseInt(req.params.id));
+  if(!todolist) res.status(404).send('manque id');
+  res.send(todolist);
+});
+
+
+/**====================================================================================================
+ * POST
+====================================================================================================*/
+app.post('/api/todolists', (req, res) => {
+  const { error } = validateTodolist(req.body); 
+
+  if (error){ // !req.body.name || req.body.name.length < 3
+    res.status(400).send(error.details[0].message)
+    return;
+  }
+// trouve le plus grand id 
+
+
+const todolist = {
+    id: todolists.length + 1,
+    name: req.body.name
+  };
+
+  todolists.push(todolist);
+  console.log(todolists.length, 'valeur dans le tableau todoLists');
+  console.log('=== ',todolists );
+  res.send(todolist)
+});
+
+
+/**====================================================================================================
+ * PUT 
+====================================================================================================*/
+app.put('/api/todolists/:id', (req, res) => {
+
+  const todolist = todolists.find( c => c.id === parseInt(req.params.id));
+
+  if(!todolist){
+    res.status(404).send('manque id');
+    return;
+  } 
+
+  const { error } = validateTodolist(req.body); 
+
+  if (error){ // !req.body.name || req.body.name.length < 3
+    res.status(400).send(error.details[0].message)
+    return;
+  }
+
+  todolist.name = req.body.name;
+  console.log("id :  " +  req.params.id + " mis a jour ");
+  res.send(todolist)
+});
+
+/**====================================================================================================
+ * Function de validation
+====================================================================================================*/
+function validateTodolist(todolist){
+
+  const schema = {
+    // il faut que le name, a minimum 3 caractère
+    name: Joi.string().min(3).required()
+  }
+
+  return Joi.validate(todolist, schema);
+}
+
+// TODO  a terminer , prend le dernier element du tableau
+// function DernierElementTableau(){
+//   Math.max.apply(Math, todolists.map(function(o) { return o.y; }))
+// }
+/**====================================================================================================
+ * API DELETE - Supprime une tache 
+====================================================================================================*/
+app.delete('/api/todolist/:id', (req, res) => {
+
+  const todolist = todolists.find( c => c.id === parseInt(req.params.id));
+  
+  // si il na pas d'id, erreur 404
+  if(!todolist){
+    res.status(404).send('manque id');
+  } 
+
+  //delete
+  
+  // La méthode indexOf() renvoie le premier indice pour lequel on trouve un élément donné dans un tableau
+  const index = todolists.indexOf(todolist);
+  // splite permet de diviser une chaine à partir d'un séparateurs
+  todolists.splice(index, 1);
+  // return la list du tableau
+  console.log("id :  " +  req.params.id + " ========== element supprimer ");
+  res.send(todolist)
+});
+
+
+
+
+
+
+
+
+/**==================================================
+ * >>>>>> CONNEXION AU SERVEUR 
+==================================================*/
+const port = 7778;
+hostname = 'localhost'; 
+app.listen(port, hostname, function () {
+    console.log("Mon serveur fonctionne sur http://"+ hostname +":"+port+"\n");
+})
+
+
+
+
+
