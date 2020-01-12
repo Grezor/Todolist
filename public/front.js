e03974590141de59f0d074aa2f70b199cc370515
Geoffrey Duplessi <grezor.gamer@gmail.com>
2020-01-12 14:25:25 +0100

    mise en forme and color

--- public/front.js
+++ public/front.js
@@ -4,7 +4,7 @@ const container = document.createElement('ul')
 container.setAttribute('id', 'incomplete-tasks')
 app.appendChild(container);
 
-var serverUrl = 'http://localhost:1472';
+const serverUrl = 'http://localhost:1472';
 // var request = new XMLHttpRequest();
 render();
 
@@ -13,12 +13,12 @@ render();
  * Afficher au chargement de la page
  */
 function render(){
-    var request = new XMLHttpRequest();
+    const request = new XMLHttpRequest();
     request.open('GET', serverUrl + '/api/todolists', true);
 
     request.onload = function(){
         if (request.status >= 200 && request.status < 400)  {
-            var data = JSON.parse(this.response);
+            const data = JSON.parse(this.response);
             refreshTodoList(data);
         }else{
             statusError();
@@ -26,15 +26,11 @@ function render(){
     }
     request.send();
 }
-/**
- * 
- */
+
 function listenerClicAdd(){
     
 }
-/**
- * 
- */
+
 function listenerClicEdit(event){
     const input = event.target.parentNode.querySelector('input[type="text"]');
     input.style.display = "block";
@@ -43,7 +39,7 @@ function listenerClicEdit(event){
 function listenerClicEditSubmit(event) {
     if (event.keyCode === 13) {
         const id = parseInt(event.target.parentNode.dataset.id)
-        var request = new XMLHttpRequest();
+        const request = new XMLHttpRequest();
 
         // requete serveur methode post
         request.open('PUT', serverUrl + '/api/todolists/' + id, true);
@@ -57,9 +53,7 @@ function listenerClicEditSubmit(event) {
         request.send(JSON.stringify({name: event.target.value}));
     }
 }
-/**
- * 
- */
+
 function listenerClicDelete(event) {
     const id = parseInt(event.target.parentNode.dataset.id)
     var request = new XMLHttpRequest();
@@ -75,48 +69,12 @@ function listenerClicDelete(event) {
 
     request.send()
 }
-/**
- * 
- */
-function listenerClicUpdate(){
-    
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 function refreshTodoList(data) {
     container.innerHTML = '';
 
     data.forEach(afaire => {
+        // creation du li
         const li = document.createElement('li');
         li.setAttribute('class', 'afaire')
         li.dataset.id = afaire.id
@@ -126,28 +84,33 @@ function refreshTodoList(data) {
 
         const label = document.createElement("LABEL");
         label.textContent = afaire.name
-        label.setAttribute('class', 'label');
+        label.setAttribute('class', 'classLabel');
+        // class css
+        label.className = 'labelColor'
 
+        // Champs input 
         const inputText = document.createElement('INPUT')
         inputText.setAttribute("type", "text");
         inputText.setAttribute('value', afaire.name);
         inputText.setAttribute('class', 'label');
+        // touche entree du clavier
         inputText.addEventListener('keyup', listenerClicEditSubmit);
-
 
         const BtnEdit = document.createElement('INPUT')
         BtnEdit.setAttribute("type", "button");
-        BtnEdit.setAttribute("class", "edit");
         BtnEdit.setAttribute("value", "EDIT");
         BtnEdit.setAttribute("id", "edit");
+        // touche entree du clavier
         BtnEdit.addEventListener('click', listenerClicEdit)
+        // class css
+        BtnEdit.className = 'buttonEdit'
 
         const BtnDelete = document.createElement('INPUT')
         BtnDelete.setAttribute("type", "button");
-        BtnDelete.setAttribute("class", "delete");
         BtnDelete.setAttribute("value", "DELETE");
         BtnDelete.addEventListener('click', listenerClicDelete)
-
+        // class css
+        BtnDelete.className = 'buttonDelete';
 
         container.appendChild(li);
         li.appendChild(input);
