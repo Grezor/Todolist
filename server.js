80a036b82d8d17187957ad5653c9574cb0de8ac1
Geoffrey Duplessi <grezor.gamer@gmail.com>
2020-09-15 20:03:47 +0200

    modifie port

--- server.js
+++ server.js
@@ -161,8 +161,8 @@ function validateTodolist(todolist) {
 /**==================================================
  * >>>>>> CONNEXION AU SERVEUR 
 ==================================================*/
-const port = process.env.PORT || 1472;
+const PORT = process.env.PORT || 5000;
 const hostname = 'localhost';
-app.listen(port, hostname, function () {
+app.listen(PORT, hostname, function () {
   console.log("Mon serveur fonctionne sur http://" + hostname + ":" + port + "\n");
 })
